20ccfc8b7bd8c86b28337371caab13b4dff9e7a3
Luke Z. <135392153+luhaza@users.noreply.github.com>
2024-10-15 21:05:52 -0400

    Add files via upload

---
+++ thought.py
@@ -0,0 +1,70 @@
+"""
+Please answer the following thought questions. These are questions that help you reflect
+on the process of building this chatbot and about ethics. 
+
+We are expecting at least three complete sentences for each question for full credit. 
+
+Each question is worth 2.5 points. 
+"""
+
+######################################################################################
+"""
+QUESTION 1 - Anthropomorphizing
+
+Is there potential for users of your chatbot possibly anthropomorphize 
+(attribute human characteristics to an object) it? 
+What are some possible ramifications of anthropomorphizing chatbot systems? 
+Can you think of any ways that chatbot designers could ensure that users can easily 
+distinguish the chatbot responses from those of a human?
+"""
+
+Q1_your_answer = """
+
+Delete and put your answer here
+
+"""
+
+######################################################################################
+
+"""
+QUESTION 2 - Privacy Leaks
+
+One of the potential harms for chatbots is collecting and then subsequently leaking 
+(advertly or inadvertently) private information. Does your chatbot have risk of doing so? 
+Can you think of ways that designers of the chatbot can help to mitigate this risk? 
+"""
+
+Q2_your_answer = """
+
+Delete and put your answer here
+
+"""
+
+######################################################################################
+
+"""
+QUESTION 3 - Classifier 
+
+When designing your chatbot, you had the choice of using a lexicon-based sentiment analysis module 
+or a statistical module (logistic regression). Which one did you end up choosing for your 
+system. Why? 
+"""
+
+Q3_your_answer = """
+
+Delete and put your answer here
+
+"""
+
+"""
+QUESTION 4 - Refelection 
+
+You just built a frame-based dialogue system using a combination of rule-based and machine learning 
+approaches. Congratulations! Reflect on the advantages and disadvantages of this paradigm. 
+"""
+
+Q4_your_answer = """
+
+Delete and put your answer here
+
+"""

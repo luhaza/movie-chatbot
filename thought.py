142400edb29d626efa062321bb2ab78d88806df1
Rick Yanashita <ryanashita15@gmail.com>
2024-10-25 12:28:36 -0400

    some questions

--- thought.py
+++ thought.py
@@ -20,7 +20,15 @@ distinguish the chatbot responses from those of a human?
 
 Q1_your_answer = """
 
-Delete and put your answer here
+I think there is potential for users of our chatbot to possibly anthropomorphize it 
+because there is a possibility that after the chatbot asks the user "How are you doing today?",
+the user could respond and then ask the chatbot how it is doing, anthropomorphizing it by asking
+something like "How are you?". Some possible ramifications of anthropomorphizing chatbot systems
+could be 1. expecting more intelligent responses out of the chatbot, and 2. becoming emotionally attached
+to the chatbot (although I think this is very unlikely to occur). One way that chatbot designers
+could ensure that users can easily distinguish the chatbot responses from those of a human would be to
+set up the chatbot's responses such that the chatbot doesn't use pronouns to address itself, and doesn't
+use adverbs like "please". Doing these things will make the chatbot's responses more robotic. 
 
 """
 
@@ -36,7 +44,13 @@ Can you think of ways that designers of the chatbot can help to mitigate this ri
 
 Q2_your_answer = """
 
-Delete and put your answer here
+No, our chatbot does not have any risks of collecting and then subsequently leaking private information. 
+This is because our chatbot doesn't ask any private information (doesn't ask for name, age, etc), and hops
+straight into movie recommendations. Designers of the chatbot can help to mitigate the risk of leaking 
+private information by insuring that the private information (if used to craft more personalized responses) will 
+never be added to datasets that the chatbot's features (in this case movie recommendations) are trained on. Additionally
+designers can make sure that all personal/private information is not saved anywhere after a singular interaction
+finishes. 
 
 """
 
@@ -52,7 +66,7 @@ system. Why?
 
 Q3_your_answer = """
 
-Delete and put your answer here
+We used the rule-based/lexicon-based sentiment analysis module. We chose this route because...
 
 """
 
@@ -65,6 +79,17 @@ approaches. Congratulations! Reflect on the advantages and disadvantages of this
 
 Q4_your_answer = """
 
-Delete and put your answer here
+The advantages of a frame-based dialogue system using a combination of rule-based and machine learning
+approaches isd that there is a clear structure, it has usecase-specific applications, it is easily modifiable/scalable,
+and it uses less data than a fully machine learning approach. The clear structure makes it easy to build (just build the
+individual components and combine them together in process), and the functions have clear objectives (not ambigious). Due 
+to the clear structure, the program is also very scalable. We can continue to create functions for features, and it will be easy 
+to incorporate those features into process. 
 
+The disadvantages of this type of system is that it has very limited ability for open-ended discussions. Users can not have a 
+back-and-forth conversation with our chatbot, compared to the extensive conversational ability of LLM chatbots like ChatGPT. There 
+is also a challenge to balance the use of rule-based and machine learning approaches. We were constantly asking ourselves which type 
+of approach would be better where... Finally, another disadvantage is that there is limited ability of the chatbot to perform things 
+other than give movie recommendations because we would have to do an overhaul of the whole system, changing if statements, creating a 
+host of new functions, etc. 
 """
